633d5571bee40d7d8e2909d7e2ab14894fe963e4
Antonia Villamil Rodriguez <anto.villamil2008@gmial.com>
2026-03-12 10:10:41 -0500

    problemas 1, 2, 3

---
+++ function_types.py
@@ -0,0 +1,32 @@
+# function_types.py
+
+def list_shift(lista, valor):
+    """
+    Modifica la lista de entrada sumando 'valor' a cada elemento.
+    No retorna nada (modificación in-place).
+    """
+    for i in range(len(lista)):
+        lista[i] += valor
+
+def calc_avg(lista):
+    """
+    Calcula y retorna la media aritmética de una lista de flotantes.
+    """
+    promedio = sum(lista) / len(lista)
+    return promedio
+
+def print_normalized(lista):
+    """
+    Imprime la lista de flotantes recibida.
+    """
+    print(lista)
+
+if __name__ == "__main__":
+    datos = [2.0, 4.0, 6.0, 8.0]
+
+    prom = calc_avg(datos)    
+    
+
+    list_shift(datos, -prom)       
+
+    print_normalized(datos)        
---
+++ functions.py
@@ -0,0 +1,9 @@
+
+def promdedio_estudiante(notas):
+    if len(notas)==0:
+        return 0.0
+    promedio=sum(notas)/len(notas)
+    return float(promedio)
+print(promdedio_estudiante([85, 92, 78]))
+
+
